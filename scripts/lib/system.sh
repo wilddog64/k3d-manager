958a938621d89df728a06a1789a3f06f28378aad
chengkai liang <chengkai.liang@costcotravel.com>
2025-09-03 21:25:41 -0700

    k3d-manager::system::_k3d_cluster_exist: flip/flop 0 and 1

--- scripts/lib/system.sh
+++ scripts/lib/system.sh
@@ -305,9 +305,9 @@ function _k3d_cluster_exist() {
    local cluster_name=$1
 
    if _run_command --no-exit -- k3d cluster list "$cluster_name" >/dev/null 2&>1 ; then
-      return 1
-   else
       return 0
+   else
+      return 1
    fi
 }
 
