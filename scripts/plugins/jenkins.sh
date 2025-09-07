83c1a3cde7cd0230cf87608cc06a38035b31d8d5
chengkai liang <chengkai.liang@costcotravel.com>
2025-09-07 09:51:24 -0700

    k3d-manager::plugins::jenkins: jenkins deployment logic implement

--- scripts/plugins/jenkins.sh
+++ scripts/plugins/jenkins.sh
@@ -1,3 +1,16 @@
+VAULT_PLUGIN="$PLUGINS_DIR/vault.sh"
+if [[ -r "$VAULT_PLUGIN" ]]; then
+   # shellcheck disable=SC1090
+   source "$VAULT_PLUGIN"
+fi
+
+JENKINS_CONFIG_DIR="$SCRIPT_DIR/etc/jenkins"
+VALUT_PLUGIN="$PLUGINS_DIR/vault.sh"
+if [[ -r "$VALUT_PLUGIN" ]]; then
+   # shellcheck disable=SC1090
+   source "$VALUT_PLUGIN"
+fi
+
 function _create_jenkins_namespace() {
    jenkins_namespace="${1:-jenkins}"
    export namespace="${jenkins_namespace}"
@@ -71,8 +84,61 @@ function _deploy_jenkins_image() {
 
 function deploy_jenkins() {
    jenkins_namespace="${1:-jenkins}"
-   jenkins_version="${2:-lts}"
 
+   deploy_vault ha
+   _create_jenkins_admin_vault_policy "vault"
    _create_jenkins_namespace "$jenkins_namespace"
-   _create_jenkins_pv_pvc "$jenkins_namespace"
+   _deploy_jenkins "$jenkins_namespace"
+}
+
+function _deploy_jenkins() {
+   local ns="${1:-jenkins}"
+
+   _helm repo add jenkins https://charts.jenkins.io
+   _helm repo update
+   _helm upgrade --install jenkins jenkins/jenkins \
+      --namespace "$ns" \
+      -f "$JENKINS_CONFIG_DIR/values.yaml"
+}
+
+function _create_jenkins_admin_vault_policy() {
+   local vault_namespace="${1:-vault}"
+
+   if _vault_policy_exists "$vault_namespace" "jenkins-admin"; then
+      _info "Vault policy jenkins-admin already exists, skip"
+      return 0
+   fi
+
+   # create policy once (idempotent)
+   cat > jenkins-admin.hcl <<'HCL' |
+   _kubectl -n "$vault_namespace" exec -i vault-0 -- \
+      vault write sys/policies/password/jenkins-admin policy=-
+   length = 24
+   rule "charset" { charset = "abcdefghijklmnopqrstuvwxyz" }
+   rule "charset" { charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+   rule "charset" { charset = "0123456789" }
+   rule "charset" { charset = "!@#$%^&*()-_=+[]{};:,.?" }
+HCL
+
+   # generate + store under KV v2 path: secret/eso/jenkins-admin
+   _kubectl -n "$vault_namespace" exec -i vault-0 -- vault write -field=password \
+      sys/policies/password/jenkins-admin/generate
+
+   _kubectl -n "$vault_namespace" exec -i vault-0 -- sh - \
+      vault kv put secret/eso/jenkins-admin \
+      username=admin password="$(_kubectl -n "$vault_namespace" exec -i vault-0 -- \
+      vault read -field=password sys/policies/password/jenkins-admin/generate)"
+
+}
+
+function _sync_vault_jenkins_admin() {
+   local vault_namespace="${1:-vault}"
+   local jenkins_namespace="${2:-jenkins}"
+   _kubectl -n "$vault_namespace" exec -i vault-0 -- vault write -field=password \
+      sys/policies/password/jenkins-admin/generate
+
+   _kubectl -n "$vault_namespace" exec -i vault-0 -- sh - \
+      vault kv put secret/eso/jenkins-admin \
+      username=admin password="$(_kubectl -n "$vault_namespace" exec -i vault-0 -- \
+      vault read -field=password sys/policies/password/jenkins-admin/generate)"
 }
