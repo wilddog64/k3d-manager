b52b6a608c56c5f0815d1fbf7f1c4b018747c167
chengkai liang <chengkai.liang@costcotravel.com>
2025-09-21 08:11:51 -0700

    k3d-manager: update cluster template export default jenkins home for mounting

--- scripts/etc/cluster_var.sh
+++ scripts/etc/cluster_var.sh
@@ -3,3 +3,5 @@ export IP=$(ip -4 route get 8.8.8.8 | perl -nle 'print $1 if /src (.*) uid/')
 # Default ports for cluster load balancer
 export HTTP_PORT="${HTTP_PORT:-8089}"
 export HTTPS_PORT="${HTTPS_PORT:-9443}"
+export JENKINS_HOME_PATH="${JENKINS_HOME_PATH:-${SCRIPT_DIR}/storage/jenkins_home}"
+export JENKINS_HOME_IN_CLUSTER="${JENKINS_HOME_IN_CLUSTER:-/data/jenkins}"
