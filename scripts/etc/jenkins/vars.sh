a97bf8c372371db56e8421b44bdbd113c1ff98af
chengkai liang <chengkai.liang@costcotravel.com>
2025-10-04 11:13:34 -0700

    k3d-manager::plugins::jenkins: add missing variables

--- scripts/etc/jenkins/vars.sh
+++ scripts/etc/jenkins/vars.sh
@@ -1,5 +1,5 @@
+export ALLOW_EMPTY_VARS=(VAULT_NAMESPACE bin)
 export JENKINS_NAMESPACE="jenkins"
-export JENKINS_HOME_PATH="$SCRIPT_DIR/storage/jenkins_home"
 export VAULT_ENABLE_INJECTOR="${VAULT_ENABLE_INJECTOR:-true}"
 export JENKINS_ENABLE_VAULT_AGENT_INJECTOR="${JENKINS_ENABLE_VAULT_AGENT_INJECTOR:-1}"
 
@@ -20,3 +20,19 @@ export JENKINS_CERT_ROTATOR_IMAGE="${JENKINS_CERT_ROTATOR_IMAGE:-docker.io/googl
 export JENKINS_CERT_ROTATOR_VAULT_ROLE="${JENKINS_CERT_ROTATOR_VAULT_ROLE:-jenkins-cert-rotator}"
 export JENKINS_CERT_ROTATOR_ALT_NAMES="jenkins.dev.local.me,jenkins.dev.k3d.internal"
 export JENKINS_VIRTUALSERVICE_HOSTS="${JENKINS_CERT_ROTATOR_ALT_NAMES}"
+export JENKINS_CERT_ROTATOR_SCRIPT_B64="$(base64 < "${SCRIPT_DIR}/etc/jenkins/cert-rotator.sh" | tr -d '\n')"
+export JENKINS_CERT_ROTATOR_VAULT_PKI_LIB_B64="$(base64 < "${SCRIPT_DIR}/lib/vault_pki.sh" | tr -d '\n')"
+
+# Vault PKI config
+export VAULT_ADDR="https://vault.vault.svc.cluster.local:8200"
+export VAULT_AUTH_PATH="pki"
+export VAULT_ROLE="jenkins-cert-rotator"
+export VAULT_PKI_ROLE_TTL="720h"
+export VAULT_NAMESPACE=""
+export VAULT_SKIP_VERIFY="true"  # true if using TLS between Vault and Jenkins
+export VAULT_CACERT="/etc/ssl/certs/vault-ca.pem"
+
+# roator specific
+export VAULT_PKI_SECRET_NS="istio-system"
+export VAULT_PKI_PATH="${VAULT_PKI_PATH:-pki}"
+export VAULT_PKI_ROLE="${VAULT_PKI_ROLE:-jenkins-tls}"
