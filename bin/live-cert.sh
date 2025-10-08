2a6acf6830905fcdf474aa40745f4284f098bdee
chengkai liang <chengkai.liang@costcotravel.com>
2025-10-08 16:47:33 -0700

    live-cert.sh: mv trap up

--- bin/live-cert.sh
+++ bin/live-cert.sh
@@ -12,9 +12,8 @@ fi
 # Dump the leaf cert actually served (no trust required)
 openssl s_client -showcerts -servername "$HOST" -connect "$IP:$PORT" </dev/null \
   | sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' | head -n 100 > /tmp/live.crt
+trap "rm -f /tmp/live.crt" EXIT
 
 # Fingerprint & SANs of the *served* cert
 openssl x509 -in /tmp/live.crt -noout \
   -subject -issuer -dates -ext subjectAltName -fingerprint -sha256
-
-trap "rm -f /tmp/live.crt" EXIT
